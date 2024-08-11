9fa8e01dea81f7641547c2b627ded0db3b3dd7f8
thang-39 <91838559+thang-39@users.noreply.github.com>
2024-08-11 11:39:44 +0700

    routing with url.parse().pathname

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+
 const config = require("./mmodule/config");
 
 function onRequest(req, res) {
-  fs.readFile('./home123.html', function(err, data) {
+  const path = url.parse(req.url).pathname;
+  console.log(path);
+
+  if (path == '/about') {
+    fs.readFile('./views/about.html', function(err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.write('File not found');
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  } else if (path == '/') {
+    fs.readFile('./views/home.html', function(err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.write('File not found');
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  }
+
+  fs.readFile('./views/home.html', function(err, data) {
     if (err) {
       res.writeHead(404);
       res.write('File not found');
